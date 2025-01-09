834f33ac86568b876babd008826a96596f4923d5
Jean8110 <jean.lin9699@icloud.com>
2025-01-09 12:30:56 +0900

    Update LINE.py

--- LINE.py
+++ LINE.py
@@ -24,7 +24,7 @@ app = Flask(__name__)
 # 設定
 CHANNEL_ACCESS_TOKEN = "lFD0fT5izkXIdZvVxg8hhpuehkxbar5utGwa7QyffB/IOLIZ5T1B5jwZnpF9STfHWdv7nbN7dDYklIjMdOU5G8LFXlVqjlC1HHsFemN+ydSYSRE9+lvaoAEdD2fNl76NVl6IhR5cE33AzdRVj+RWuQdB04t89/1O/w1cDnyilFU="
 GROUP_IDS = [
-    "C190c4556db345f1cc094590dc96b7c99”, # 正式群組
+    "C190c4556db345f1cc094590dc96b7c99” # 正式群組
 ]
 FOLDER_ID = "1yNH3mP2LAUnZn8EjjGrzzpzNavkSzMqB"
 
